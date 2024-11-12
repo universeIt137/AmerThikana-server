4a0c00e99d243aa9990dd02978ec51f65f3b5b09
ashikur <ashikur1602@gmail.com>
2024-11-12 15:49:22 +0600

    content related api done

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware  
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.olinusx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server    (optional starting in v4.7)
+    await client.connect();
+
+    const contentCollection = client.db('AmerThikana').collection('websiteContent'); //change
+
+    app.post('/content', async (req, res) => {
+      const data = req.body;
+      const result = await contentCollection.insertOne(data);
+      res.send(result);
+    })
+
+    app.get('/content', async (req, res) => {
+      const result = await contentCollection.find().toArray();
+
+      res.send(result);
+    })
+
+    app.get('/content/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contentCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.put('/content/:id', async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedInfo = {
+        $set: {
+          ...data
+        }
+      }
+
+      const result = await contentCollection.updateOne(query, updatedInfo, options);
+      res.send(result);
+    })
+
+    app.delete('/content/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contentCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('server is ok');
+});
+
+app.listen(port, () => {
+  console.log(`sever is running on port ${port}`);
+});
