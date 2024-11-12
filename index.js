5c9bc857cb62ac84c0d2431f4be028efc54d7313
ashikur <ashikur1602@gmail.com>
2024-11-12 18:27:50 +0600

    some changes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server    (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const contentCollection = client.db('AmerThikana').collection('websiteContent');
     const chairmanCollection = client.db('AmerThikana').collection('chairman');
@@ -117,8 +117,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -131,5 +131,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`sever is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
