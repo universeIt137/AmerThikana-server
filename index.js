4f8642f11105e651155977e08f1f700cade4c149
ishanrana363 <ishanrana363@gmail.com>
2024-11-18 12:33:25 +0600

    schedule find by id api created

--- index.js
+++ index.js
@@ -282,6 +282,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/schedule/:id",async(req,res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await scheduleCollection.findOne(query);
+      res.send(result);
+    });
+
 
 
 
